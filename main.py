64ef260d717d17405ef6de5dd429e90491ded954
RatSpaceSteam <jacobalexanderpatt@gmail.com>
2023-09-29 10:30:40 -0500

    questions
    "Looking into this"

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
 from kivy.app import App
-from kivy.uix
-class LoginPage(App):
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen, ScreenManager
+
+Builder.load_file("LoginPage.kv")
+
+class LoginPageApp(App):
+    def build(self):
+        return LoginPageManager()
+
+class LoginPageManager(ScreenManager):
+    pass
+
+class BoxLayoutExample(BoxLayout):
+    pass
+class Question1Screen(Screen):
+    def answer_question(self, bool):
+        if bool:
+            self.manager.current = "correct"
+        else:
+            self.manager.current = "error"
+
+class Question2Screen(Screen):
+    def answer_question(self, bool):
+        if bool:
+            self.manager.current = "correct"
+        else:
+            self.manager.current = "error"
+
+class CorrectScreen(Screen):
+    pass
+
+class ErrorScreen(Screen):
+    pass
+if __name__ == "__main__":
+    LoginPageApp().run()
